c30e6f3f8ddb16179db4dd0e5a8b6ca7df9037ef
cesc <cscherhoff@web.de>
2021-12-07 23:44:35 +0100

    Initial commit

---
+++ src/main/java/com/exxeta/expenseservice/ExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exxeta.expenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exxeta/expenseservice/ExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exxeta.expenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
